78d3678cbb5e2ba79daa0dac4700ac4bcf94728b
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2022-12-13 01:39:20 -0800

    Ver1.2 Database Model
    Collection
    Movie
    MovieCast
    MovieCollection
    MovieCrew

---
+++ Models/Database/Movie.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using NeoMovie.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoMovie.Models.Database
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        //The movieID from TMDB. 
+        public int MovieId { get; set; }
+
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public string TagLine { get; set; }
+        public string Overview { get; set; }
+        public int RunTime { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public MovieRating Rating { get; set; }
+
+        public float VoteAverage { get; set; }
+
+        [Display(Name = "Poster")]
+        public byte[] Poster { get; set; }
+        public string PosterType { get; set; }
+
+        [Display(Name = "Backdrop")]
+        public byte[] Backdrop { get; set; }
+        public string BackdropType { get; set; }
+
+        public string TrailerUrl { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Poster Image")]
+        public IFormFile PosterFile { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Backdrop Image")]
+        public IFormFile BackdropFile { get; set; }
+
+        public ICollection<MovieCollection> MovieCollections { get; set; } = new HashSet<MovieCollection>();
+        public ICollection<MovieCast> Cast { get; set; } = new HashSet<MovieCast>();
+        public ICollection<MovieCrew> Crew { get; set; } = new HashSet<MovieCrew>();
+    }
+}
---
+++ Models/Database/MovieCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoMovie.Models.Database
+{
+    public class MovieCollection // Linking Table for Movies & Collections
+    {
+        public int Id { get; set; }
+        public int CollectionId { get; set; }
+        public int MovieId { get; set; }
+
+        public int Order { get; set; }
+
+        public virtual Collection Collection { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
