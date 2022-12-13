bdf3ce1a52b0f0d225fa908fc97e6480bb47d27a
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2022-12-13 02:01:24 -0800

    Ver1.4 Interface Services
    IRemoteMovieService
    IDataMappingService
    IImageService

---
+++ Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,16 @@
+﻿using NeoMovie.Enums;
+using NeoMovie.Models.TMDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoMovie.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetail> MovieDetailAsync(int id);
+        Task<MovieSearch> SearchMoviesAsync(MovieCategory category, int count);
+        Task<ActorDetail> ActorDetailAsync(int id);
+    }
+}
