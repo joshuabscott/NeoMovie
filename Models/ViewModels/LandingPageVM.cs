18e0f3d335fe9fdf67bf53e5c271ff4d20ca3d1d
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2022-12-13 01:46:06 -0800

    Ver1.3 ViewModels
    LandingPage

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,18 @@
+﻿using NeoMovie.Models.Database;
+using NeoMovie.Models.TMDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoMovie.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
