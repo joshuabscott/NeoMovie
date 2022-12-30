70ccfbe68ebba2eef24f651d467219e543c2167f
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2022-12-30 10:14:45 -0800

    Ver2.9
    Update Seed Service

---
+++ Migrations/20221230181131_02_Add_Collection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NeoMovie.Migrations
+{
+    public partial class _02_Add_Collection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
